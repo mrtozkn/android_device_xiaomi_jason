c7acd9e072519be1ceec57e1f136368187e3d28a
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    jason: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- device.mk
+++ device.mk
@@ -118,7 +118,7 @@ PRODUCT_SHIPPING_API_LEVEL := 25
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # VNDK
 PRODUCT_PACKAGES += \
