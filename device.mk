eb5cf68eeec1b142188ffcb5795c055fd3c81493
Mert Ozkan <mertyozkan@trakya.edu.tr>
2024-10-01 00:07:51 +0300

    jason: keymaster update

--- device.mk
+++ device.mk
@@ -73,8 +73,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@4.1 \
+    android.hardware.keymaster@4.1-service
 
 # Lineage hardware
 PRODUCT_PACKAGES += \
